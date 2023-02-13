3f2ed1ae2e4864aa1fae40d3a229cb361a1b2299
2193560021 <62364555+2193560021@users.noreply.github.com>
2023-02-13 19:59:14 +0800

    使用Webpack打包ts代码1

---
+++ code/chapter01/part3/webpack.config.js
@@ -0,0 +1,47 @@
+// 引入一个包
+const path = require('path');
+
+// 引入html插件
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+//webpack中的所有配置信息都应该写在module.exports中
+module.exports = {
+
+    //指定入口文件
+    entry:'./src/index.ts',
+
+    // 指定打包文件所在目录
+    output:{
+        // 指定打包文件目录
+        path:path.resolve(__dirname,'dist'),
+
+        //打包文件的文件名
+        filename:"bundle.js"
+    },
+
+    // 指定webpack打包时要使用模块
+    module:{
+        //指定要加载的规则
+        rules:[
+            {
+                // test指定的是规则生效文件
+                test:/\.ts$/,
+                //要使用的loader
+                use:'ts-loader',
+                //要排除的文件
+                exclude:/node_modules/
+            }
+        ]
+    },
+
+    //配置webpack插件
+    plugins:[
+        new HTMLWebpackPlugin({
+            title:"LYY-TypeScript"
+        }),
+    ]
+
+
+
+
+}
